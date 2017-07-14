778a947fb1d60434a72c1b17740ebe2e9d7a22f0
homeless2010 <15594821882@163.com>
2017-07-14 16:52:24 +0800

    第三次提交

--- Base_data_type/dataType.ts
+++ Base_data_type/dataType.ts
@@ -21,3 +21,31 @@ document.body.innerHTML = sss;
 //数组泛型
 let list: Array<number> = [1,2,3];
 console.log(list);
+
+//元组 Tuple
+/*
+元组类型允许表示一个已知元素数量和类型的数组，各元素的类型不必相同。
+*/
+let x: [string,number];
+x = ['hello',10];
+//x = [10,'hello'];
+//当访问一个越界的元素，会使用联合类型替代。
+x[5] = 'world';
+console.log(x[5].toString());
+console.log(x[4]);
+
+console.log(x[0].substr(1));
+//console.log(x[1].substr(1));//error
+
+//枚举
+/*enum Color {Red = 1,Green,Blue}
+let c: Color = Color.Green;
+console.log(c);*/
+
+/*enum Color{Red = 1,Green =2,Blue = 4}
+let c: Color = Color.Green;
+console.log(c);*/
+
+enum Color {Red = 1,Green,Blue}
+let colorName: string = Color[2];
+alert(colorName);
